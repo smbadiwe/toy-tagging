65a444b1313701a0639729177d9b930b799f71ba
Soma Mbadiwe <somasystemsng@gmail.com>
2020-05-11 03:46:43 -0400

    Fiixng imports

--- models/lstm_tagger.py
+++ models/lstm_tagger.py
@@ -1,23 +1,12 @@
-from typing import Iterator, List, Dict
+from typing import Dict
+
 import torch
-import torch.optim as optim
-import numpy as np
-from allennlp.data import Instance
-from allennlp.data.fields import TextField, SequenceLabelField
-from allennlp.data.dataset_readers import DatasetReader
-from allennlp.common.file_utils import cached_path
-from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token
 from allennlp.data.vocabulary import Vocabulary
 from allennlp.models import Model
-from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
-from allennlp.modules.token_embedders import Embedding
-from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder, PytorchSeq2SeqWrapper
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
+from allennlp.modules.text_field_embedders import TextFieldEmbedder
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
 from allennlp.training.metrics import CategoricalAccuracy
-from allennlp.data.iterators import BucketIterator
-from allennlp.training.trainer import Trainer
-from allennlp.predictors import SentenceTaggerPredictor
 
 
 class LstmTagger(Model):
@@ -31,6 +20,7 @@ class LstmTagger(Model):
         self.hidden2tag = torch.nn.Linear(in_features=encoder.get_output_dim(),
                                           out_features=vocab.get_vocab_size('labels'))
         self.accuracy = CategoricalAccuracy()
+
     def forward(self,
                 sentence: Dict[str, torch.Tensor],
                 labels: torch.Tensor = None) -> Dict[str, torch.Tensor]:
@@ -44,5 +34,6 @@ class LstmTagger(Model):
             output["loss"] = sequence_cross_entropy_with_logits(tag_logits, labels, mask)
 
         return output
+
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         return {"accuracy": self.accuracy.get_metric(reset)}
--- run.py
+++ run.py
@@ -1,23 +1,15 @@
-from typing import Iterator, List, Dict
+import torch
+import numpy as np
 import torch
 import torch.optim as optim
-import numpy as np
-from allennlp.data import Instance
-from allennlp.data.fields import TextField, SequenceLabelField
-from allennlp.data.dataset_readers import DatasetReader
 from allennlp.common.file_utils import cached_path
-from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token
-from allennlp.data.vocabulary import Vocabulary
-from allennlp.models import Model
-from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
-from allennlp.modules.token_embedders import Embedding
-from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder, PytorchSeq2SeqWrapper
-from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
-from allennlp.training.metrics import CategoricalAccuracy
 from allennlp.data.iterators import BucketIterator
-from allennlp.training.trainer import Trainer
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
+from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
+from allennlp.modules.token_embedders import Embedding
 from allennlp.predictors import SentenceTaggerPredictor
+from allennlp.training.trainer import Trainer
 
 from dataset_readers.posdatareader import PosDatasetReader
 from models.lstm_tagger import LstmTagger
